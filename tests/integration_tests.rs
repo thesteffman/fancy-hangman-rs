b4e3804c3a3f70b395167bccb35c821381670b10
thesteffman <stefan.poindl@gmail.com>
2022-06-04 23:39:44 +0200

    #1 add integration tests for TextWordBase

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,13 +1,41 @@
 use fhcli::util::io::*;
 use crate::tools::{fill, get_sample_words, setup, teardown};
 
+
+#[test]
+fn test_create_word() {
+    let file_path = setup();
+
+    let word_base = TextWordBase::new(file_path.clone());
+    word_base.create_word("rusty");
+
+    teardown(file_path);
+}
+
+#[test]
+fn test_find_word() {
+    let file_path = setup();
+    fill(&file_path, get_sample_words());
+
+    let word_base = TextWordBase::new(file_path.clone());
+
+    for word_str in get_sample_words() {
+        match word_base.find_word(word_str) {
+            Some(word) => assert_eq!(word_str, word.word),
+            None => assert!(false)
+        }
+    }
+
+    teardown(file_path);
+}
+
 #[test]
 fn test_read_random_word() {
     let file_path = setup();
 
-    fill(file_path.as_str(), get_sample_words());
+    fill(&file_path, get_sample_words());
 
-    let word_base = TextWordBase::new(file_path);
+    let word_base = TextWordBase::new(file_path.clone());
 
     match word_base.get_random_word() {
         Some(word) =>
@@ -15,17 +43,7 @@ fn test_read_random_word() {
         None => assert!(false)
     }
 
-    teardown(word_base.wordbase_file_path);
-}
-
-#[test]
-fn test_create_word() {
-
-}
-
-#[test]
-fn test_find_word() {
-
+    teardown(file_path);
 }
 
 mod tools {
@@ -35,27 +53,22 @@ mod tools {
     use uuid::Uuid;
 
     pub fn setup() -> String {
-        let mut tmp_dir = temp_dir();
-        let tmp_file_name = format!("{}.txt", Uuid::new_v4());
-        tmp_dir.push(&tmp_file_name);
+        let tmp_file_name = format!("{}/{}.txt", temp_dir().to_str().unwrap(), Uuid::new_v4());
 
-        File::create(tmp_dir);
+        File::create(&tmp_file_name).unwrap();
 
-        String::from(tmp_dir.as_os_str())
+        tmp_file_name
     }
 
-    pub fn fill(file_path: &str, sample_words: Vec<&str>) {
-        let mut tmp_dir = temp_dir();
-        tmp_dir.push(file_path);
-
+    pub fn fill(file_path: &String, sample_words: Vec<&str>) {
         let file_result =  OpenOptions::new()
             .append(true)
-            .open(tmp_dir);
+            .open(file_path);
 
         match file_result {
             Ok(mut file) => {
                 for word in sample_words {
-                    file.write(word.as_ref()).unwrap();
+                    file.write(format!("{}\n", word).as_ref()).unwrap();
                 }
             }
             Err(e) => panic!("Error setting up integration test:\n{}", e)
@@ -67,6 +80,6 @@ mod tools {
     }
 
     pub fn teardown(file_path: String) {
-        remove_file(file_path);
+        remove_file(file_path).unwrap();
     }
 }
